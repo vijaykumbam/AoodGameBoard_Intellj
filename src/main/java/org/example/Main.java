7119c760dcfbe1801be236c2ca08ebb9d6eeac78
msidemo-admin <kumbam96@students.rowan.edu>
2023-02-28 14:49:43 -0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.hello.HelloWorld;
+
+public class Main {
+    public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.printHelloWorld();
+    }
+}
---
+++ src/main/java/org/example/hello/HelloWorld.java
@@ -0,0 +1,8 @@
+package org.example.hello;
+
+public class HelloWorld {
+
+    public void printHelloWorld(){
+        System.out.println("Hello World");
+    }
+}
